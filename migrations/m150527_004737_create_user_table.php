b39846dd143027f75ce0585f7b6f32d4e9a109bd
maxshabl <maxshabl@yandex.ru>
2017-05-03 17:31:07 +0300

    first commit

---
+++ migrations/m150527_004737_create_user_table.php
@@ -0,0 +1,25 @@
+<?php
+use yii\db\Schema;
+use yii\db\Migration;
+class m150527_004737_create_user_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable('sh_users', [
+            'id' => Schema::TYPE_PK,
+            'username' => Schema::TYPE_STRING.' NOT NULL',
+            'email' => Schema::TYPE_STRING.' NOT NULL',
+            'password_hash' => Schema::TYPE_STRING.' NOT NULL',
+            'status' => Schema::TYPE_SMALLINT.' NOT NULL',
+            'auth_key' => Schema::TYPE_STRING.'(32) NOT NULL',
+            'created_at' => Schema::TYPE_INTEGER.' NOT NULL',
+            'updated_at' => Schema::TYPE_INTEGER.' NOT NULL',
+        ]);
+    }
+    public function down()
+    {
+        $this->dropTable('sh_users');
+    }
+
+
+}
